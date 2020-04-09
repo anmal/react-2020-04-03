b51ac71df0b2b78b1aa5edbadbd2f6890ed28ca8
Анна Малышева <a.malysheva@aamsystems.ru>
2020-04-09 16:17:05 +0300

    Updates

---
+++ precache-manifest.e40bc6233f2008d50a0007ec73107187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16d1b86e956e8e6765a916925b0b8aa",
+    "url": "/react-homework1/index.html"
+  },
+  {
+    "revision": "68b681fd138acde02cf4",
+    "url": "/react-homework1/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "5c942fb1d9e47fd01ad7",
+    "url": "/react-homework1/static/css/main.1881e2d1.chunk.css"
+  },
+  {
+    "revision": "68b681fd138acde02cf4",
+    "url": "/react-homework1/static/js/2.e269e35b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-homework1/static/js/2.e269e35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c942fb1d9e47fd01ad7",
+    "url": "/react-homework1/static/js/main.94eff6b5.chunk.js"
+  },
+  {
+    "revision": "1f258e0942d14d095f0c",
+    "url": "/react-homework1/static/js/runtime-main.72837700.js"
+  }
+]);
